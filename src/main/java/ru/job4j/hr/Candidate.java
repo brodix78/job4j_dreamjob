35b131784507111f6fef0a36c853784f75056163
brodix78 <brodnikov1950@gmail.com>
2020-07-23 21:27:46 +0400

    First commit from IDEA

---
+++ src/main/java/ru/job4j/hr/Candidate.java
@@ -0,0 +1,16 @@
+package ru.job4j.hr;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Candidate {
+    private String name;
+    private String surname;
+    private String thirdName;
+    private long birthDate;
+    private String sex;
+    private LinkedList<String> specialities;
+    private List<String> education;
+    private LinkedList<String> experience;
+    private String contact;
+}
---
+++ src/main/java/ru/job4j/hr/Officer.java
@@ -0,0 +1,8 @@
+package ru.job4j.hr;
+
+public class Officer {
+    private String name;
+    private String surname;
+    private String thirdName;
+    private String contact;
+}
