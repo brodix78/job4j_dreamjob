ee4278d11f1705eb43ad83874568c42fabe444bd
brodix78 <brodnikov1950@gmail.com>
2020-07-26 20:31:42 +0400

    CandidateSerlvet. Создание кандидата.[2513#304082]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -13,6 +13,7 @@ public class Store {
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
     private static AtomicInteger POST_ID = new AtomicInteger(4);
+    private static AtomicInteger Candidate_ID = new AtomicInteger(4);
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
@@ -26,6 +27,11 @@ public class Store {
     public void savePost(Post post) {
         post.setId(POST_ID.incrementAndGet());
         posts.put(post.getId(), post);
+    }
+
+    public void saveCandidate(Candidate candidate) {
+        candidate.setId(POST_ID.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
     }
 
     public static Store instOf() {
--- src/main/webapp/post/edit.jsp
+++ src/main/webapp/post/edit.jsp
@@ -38,7 +38,7 @@
                 <form action="<%=request.getContextPath()%>/post/save" method="post">
                     <div class="form-group">
                         <label>Имя</label>
-                        <input type="text" class="form-control" name = "name">
+                        <input type="text" class="form-control" name="name">
                     </div>
                     <button type="submit" class="btn btn-primary">Сохранить</button>
                 </form>
