f7181d52220e4e33cff1a717687354b45a71597f
brodix78 <brodnikov1950@gmail.com>
2020-08-02 10:27:19 +0400

    PsqlStore. [281226#304053]

---
+++ src/main/java/ru/job4j/dream/store/PsqlMain.java
@@ -0,0 +1,29 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+
+public class PsqlMain {
+    public static void main(String[] args) {
+        Store store = PsqlStore.instOf();
+        store.savePost(new Post(0, "Java Job"));
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+        store.savePost(new Post(1, "Some job"));
+        store.savePost(new Post(0, "Java Job Changed"));
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+
+        store.saveCandidate(new Candidate(0, "Java jun"));
+        for (Candidate candidate : store.findAllCandidates()) {
+            System.out.println(candidate.getId() + " " + candidate.getName());
+        }
+        store.saveCandidate(new Candidate(1, "Java mid"));
+        store.saveCandidate(new Candidate(0, "Java tester"));
+        for (Candidate candidate : store.findAllCandidates()) {
+            System.out.println(candidate.getId() + " " + candidate.getName());
+        }
+    }
+}
--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -4,57 +4,15 @@ import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class Store {
-    private static final Store INST = new Store();
-    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
-    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
-    private static AtomicInteger POST_ID = new AtomicInteger(4);
-    private static AtomicInteger Candidate_ID = new AtomicInteger(4);
+public interface Store {
+    Collection<Post> findAllPosts();
 
-    private Store() {
-        posts.put(1, new Post(1, "Junior Java Job"));
-        posts.put(2, new Post(2, "Middle Java Job"));
-        posts.put(3, new Post(3, "Senior Java Job"));
-        candidates.put(1, new Candidate(1, "Junior Java"));
-        candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
-    }
+    Collection<Candidate> findAllCandidates();
 
-    public void savePost(Post post) {
-        if (post.getId() == 0) {
-            post.setId(POST_ID.incrementAndGet());
-        }
-        posts.put(post.getId(), post);
-    }
+    void savePost(Post post);
 
-    public Post postById(int id) {
-        return posts.get(id);
-    }
+    void saveCandidate(Candidate candidate);
 
-    public void saveCandidate(Candidate candidate) {
-        if (candidate.getId() == 0) {
-            candidate.setId(POST_ID.incrementAndGet());
-        }
-        candidates.put(candidate.getId(), candidate);
-    }
-
-    public Candidate candidateById(int id) {
-        return candidates.get(id);
-    }
-
-    public static Store instOf() {
-        return INST;
-    }
-
-    public Collection<Post> findAllPosts() {
-        return posts.values();
-    }
-
-    public Collection<Candidate> findAllCandidates() {
-        return candidates.values();
-    }
+    Post findByID(int id);
 }
